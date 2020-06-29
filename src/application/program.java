1f850e64134f86689b3f44c12f51e5e26539198d
ViniNepo <48017823+ViniNepo@users.noreply.github.com>
2020-06-29 17:21:04 -0300

    boardGame

---
+++ src/application/program.java
@@ -0,0 +1,13 @@
+package application;
+
+import boardGame.Position;
+
+public class program {
+
+    public static void main(String [] args) {
+
+        Position position = new Position(3, 5);
+
+        System.out.println(position);
+    }
+}
